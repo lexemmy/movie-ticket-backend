4e9a043229d267b6539060a3e3402ff2f841ab47
Eyouel <eyouelaya@gmail.com>
2023-10-23 23:31:28 -0500

    added cookies parser and cookies setting to the auth controller, middleware and app.js

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-const cors = require('cors')
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const port = process.env.PORT || 3000
-const indexRouter = require('./routes/index')
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const indexRouter = require("./routes/index");
+const cookieParser = require("cookie-parser");
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+const corsOptions = {
+  origin: true, //included origin as true
+  credentials: true, //included credentials as true
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('MongoDB Connected'))
-  .catch((err) => console.log(err))
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-app.use('/', indexRouter)
+app.use("/", indexRouter);
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,18 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 exports.hasAuth = (req, res, next) => {
-  const authHeader = req.headers['authorization']
+  const authHeader = req.cookies.token;
+  console.log(authHeader);
   if (authHeader == null) {
-    return res.status(401).json({ error: 'Authorization header missing' })
+    return res.status(401).json({ error: "Authorization header missing" });
   }
 
   jwt.verify(authHeader, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.status(401).json({ error: 'Invalid token' })
+      return res.status(401).json({ error: "Invalid token" });
     }
-    req.user = user
+    req.user = user;
 
-    next()
-  })
-}
+    next();
+  });
+};
