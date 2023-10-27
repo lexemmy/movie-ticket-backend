42c4cb439054ade32dfaa4c61941c410e916f04d
Eyouel <eyouelaya@gmail.com>
2023-10-26 22:43:49 -0500

    fix add images in movies and serach function

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static("public"));
 
 mongoose
   .connect(process.env.MONGODB_URI, {
