035d5da193ef6076d7c27518cdbd650c9006469c
lex <emmy007.el@gmail.com>
2023-10-28 13:14:19 -0500

    refactor auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,17 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
 
 exports.hasAuth = (req, res, next) => {
-  let authHeader = req.get("authorization");
-  authHeader = authHeader.split("=")[1];
+  let authHeader = req.get('authorization')
   if (!authHeader) {
-    return res.status(401).json({ error: "Authorization header missing" });
+    return res.status(401).json({ error: 'Authorization header missing' })
   }
 
   jwt.verify(authHeader, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.status(401).json({ error: "Invalid token" });
+      return res.status(401).json({ error: 'Invalid token' })
     }
-    req.user = user;
+    req.user = user
 
-    next();
-  });
-};
+    next()
+  })
+}
